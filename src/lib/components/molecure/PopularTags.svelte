6db4e71a91ea88a095cb018174278974cd4d2453
direboar <direboar@example.com>
2023-08-28 16:31:29 +0000

    get posts

--- src/lib/components/molecure/PopularTags.svelte
+++ src/lib/components/molecure/PopularTags.svelte
@@ -12,12 +12,5 @@
     {#each tags as tag}
     <a href="" class="tag-pill tag-default">{tag}</a>
     {/each}
-    <!-- <a href="" class="tag-pill tag-default">javascript</a>
-    <a href="" class="tag-pill tag-default">emberjs</a>
-    <a href="" class="tag-pill tag-default">angularjs</a>
-    <a href="" class="tag-pill tag-default">react</a>
-    <a href="" class="tag-pill tag-default">mean</a>
-    <a href="" class="tag-pill tag-default">node</a>
-    <a href="" class="tag-pill tag-default">rails</a> -->
     </div>
 </div>
